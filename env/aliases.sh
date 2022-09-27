c54db6b15d8585032b58f6624120ad2c9eee9ea4
fanglm <ooohmydawn@hotmail.com>
2022-09-27 16:02:52 +0800

    chore: init

---
+++ env/aliases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# For faster navigation
+alias ~="cd ~" # `cd` is probably faster to type though
+
+# Shortcuts
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
---
+++ mac/brew-essentials.sh
@@ -0,0 +1,22 @@
+# Install Homebrew (if not installed)
+echo "Installing Homebrew."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Installs Casks
+brew tap homebrew/cask
+
+## Apps I use
+brew install fish
+brew install nvm 
+
+# Remove outdated versions from the cellar.
+brew cleanup
